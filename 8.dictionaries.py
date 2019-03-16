47b0a23b344f4467f2febd54f2d50f002f163d9d
Avimunk1 <Avimunk@gmail.com>
2019-03-16 23:03:41 +0200

    using sub directory for input
    using lower to avoid duplication

--- 8.dictionaries.py
+++ 8.dictionaries.py
@@ -2,19 +2,28 @@
 
 
 dicCountnames=dict()
-
-names=open('inputnames.txt')
+countChar=dict()
+#names = open('inputs'+"/inputnames.txt")
+fpath = ('inputs'+"/inputnames.txt")
+#print('names=',names)
+#print('fpath=',fpath)
+names=open(fpath)
 
 
 for name in names:
     name=name.strip()
+    name=name.lower()
     if len(name)<2:
         continue
-
     if name not in dicCountnames:
         dicCountnames[name]=1
     else:
         dicCountnames[name]=dicCountnames[name]+1
+        for letter in name:
+            if letter not in countChar:
+                countChar[letter]=1
+            else:
+                countChar[letter]=countChar[letter]+1
 
-
+print(countChar)
 print(dicCountnames)
